633e41c884121b38963d628ba88857f89794d25c
AlexTran0899 <alextran0899@gmail.com>
2026-04-02 13:30:09 -0500

    04-cutom application properties

--- src/main/java/com/__springboot/demo/HelloworldController.java
+++ src/main/java/com/__springboot/demo/HelloworldController.java
@@ -6,16 +6,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-
-
 @RestController
 public class HelloworldController {
-    @Value("${creatorName}")
+    @Value("${creator.Name}")
     private String creatorName;
+
+    @Value("${creator.birthCity}")
+    private String creatorBirthCity;
+
+    @Value("${creator.birthCountry}")
+    private String creatorBirthCountry;
 
     @GetMapping("/")
     public String sendHelloWorld() {
         return "hello world from " + creatorName;
+    }
+
+    @GetMapping("/birth-city")
+    public String sendBrithCity() {
+        return creatorName + " was borned in the province of " + creatorBirthCity;
+    }
+
+    @GetMapping("/birth-country")
+    public String sendBirthCountry() {
+        return creatorName + " was borned in the country " + creatorBirthCountry;
     }
 
     @GetMapping("/fortune")
