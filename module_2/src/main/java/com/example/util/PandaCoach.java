67d909017151d7241e9c4bce991cfeba3be58795
AlexTran0899 <alextran0899@gmail.com>
2026-04-07 11:02:36 -0500

    this will lazy load everything

--- module_2/src/main/java/com/example/util/PandaCoach.java
+++ module_2/src/main/java/com/example/util/PandaCoach.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Lazy
 public class PandaCoach implements Coach {
     @Override
     public String getDailyWorkout(){
