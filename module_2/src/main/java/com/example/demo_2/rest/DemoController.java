19646a10f7ddc868d6b7cd87ee6e853687fe0caa
AlexTran0899 <alextran0899@gmail.com>
2026-04-07 17:19:52 -0500

    singleton qualifier

--- module_2/src/main/java/com/example/demo_2/rest/DemoController.java
+++ module_2/src/main/java/com/example/demo_2/rest/DemoController.java
@@ -9,16 +9,19 @@ import com.example.util.Coach;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @RestController
 public class DemoController {
-    
+
     private Coach myCoach;
+    private Coach anotherCoach;
 
     // notice that the class name CricketCoach start with the lowercase
     @Autowired
-    public void setCoach(@Qualifier("trackCoach") Coach theCoach) {
-        myCoach = theCoach;
+    public void setCoach(@Qualifier("trackCoach") Coach theCoach,
+            @Qualifier("trackCoach") Coach anotherCoach) {
+                
+        this.myCoach = theCoach;
+        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/dailyworkout")
