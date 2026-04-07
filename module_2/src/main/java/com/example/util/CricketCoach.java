932b2eac88bf412f835df8b1a046928afce7c201
AlexTran0899 <alextran0899@gmail.com>
2026-04-07 18:58:17 -0500

    add postConstruct and preDestroy method for class

--- module_2/src/main/java/com/example/util/CricketCoach.java
+++ module_2/src/main/java/com/example/util/CricketCoach.java
@@ -3,6 +3,9 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
 public class CricketCoach implements Coach {
 
@@ -10,4 +13,15 @@ public class CricketCoach implements Coach {
     public String getDailyWorkout() {
         return "Practice fast bowling fo 15 minutes";
     }
+
+    @PostConstruct
+    public void doStartupStuff(){
+        System.out.println("doing startup stuff for (" + getClass().getSimpleName() + ')');
+    }
+
+    @PreDestroy
+    public void runUponClose(){
+        System.out.println("doing closing stuff for (" + getClass().getSimpleName() + ')');
+    }
+
 }
